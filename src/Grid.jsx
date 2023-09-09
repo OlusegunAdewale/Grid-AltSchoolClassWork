1d38781f7ffcbc01ae0aaff5993f64fb0338bb16
OlusegunAdewale <adewalesegun76@gmail.com>
2023-09-09 01:07:31 +0100

    3 files modified

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,7 +1,19 @@
 import { range } from './utils';
 
 function Grid({ numRows, numCols }) {
-  return <div className="grid">{/* TODO */}</div>;
+  return (
+    <div className="grid">
+      {range(numRows).map((rowIndex) => (
+        <div key={`row-${rowIndex}`} className="grid-row">
+          {range(numCols).map((colIndex) => (
+            <div key={`col-${colIndex}`} className="grid-cell">
+              {/* Render content for each cell here */}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Grid;
--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,14 @@
-export const range = (start, end, step = 1) => {
+export const range = (start, end = start, step = 1) => {
   let output = [];
-  // solve this range utility
 
-  // handle scenario where end is not specified range(5) - [0, 1, 2, 3, 4]
-  // a conditional check for end
+  // Handle scenario where end is not specified (range(5) - [0, 1, 2, 3, 4])
+  if (end === start) {
+    end = start;
+    start = 0;
+  }
 
-  for (let i = start; i < end; i += step) {
-    // loop code
+  for (let i = start; i <= end; i += step) {
+    output.push(i);
   }
 
   return output;
